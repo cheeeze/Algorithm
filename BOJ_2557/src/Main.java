41c15a5ffdad0556048ffd1b1ff622c8c6132d79
신혜진(Hyejin Shin) <hj951219@gmail.com>
2020-10-08 23:54:14 +0900

    Hello World

---
+++ BOJ_2557/src/Main.java
@@ -0,0 +1,14 @@
+/*
+ * BOJ #2557 Hello World
+ * 시작일 : 2020-10-08
+ * 완료일 : ``
+ * 출력
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
