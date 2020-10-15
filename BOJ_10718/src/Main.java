01520db226f23a8527c5aa917f41dd5a44baeced
cheeeze <hj951219@gmail.com>
2020-10-15 23:56:27 +0900

    We love kriii

---
+++ BOJ_10718/src/Main.java
@@ -0,0 +1,14 @@
+/*
+ * BOJ #10718 We love kriii
+ * 시작일 : 2020-10-15
+ * 완료일 : ``
+ * 출력
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군\n강한친구 대한육군");
+
+	}
+
+}
